bd90f8dd6586c6339f39efee760ccd45bf1c81de
Jason_Shu <shuyue524@gmail.com>
2019-05-15 09:13:04 +0800

    使用模板html

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // 使用html模板
+
 module.exports = {
     entry: path.resolve(__dirname, 'src/main.js'),
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
         publicPath: '/'
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src/index.html')
+        })
+    ]
 }
