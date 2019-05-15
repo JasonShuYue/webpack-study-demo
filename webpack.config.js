9ccc3083a8b45e9736fc59a5b58bf527e180393a
Jason_Shu <shuyue524@gmail.com>
2019-05-15 09:34:16 +0800

    配置webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -13,5 +13,10 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'src/index.html')
         })
-    ]
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        port: '8080',
+        host: 'localhost'
+    }
 }
